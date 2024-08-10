bd40b4d470c705278fae93c4cce21fdf190b7022
schufot <156847189+schufot@users.noreply.github.com>
2024-08-10 14:36:33 +0200

    add mobile theme switcher version

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import Logo from './Logo';
 import { ThemeSwitcherButton } from './ThemeSwitcherButton';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import { Button } from './ui/button';
+import { Menu } from 'lucide-react';
 
 function Navbar() {
   return (<>
     <DesktopNavbar />
+    <MobileNavbar/>
   </>);
 }
 
@@ -22,6 +27,22 @@ function DesktopNavbar(){
             </nav>
         </div>
     );
+}
+
+function MobileNavbar(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    return <div className="block border-separate bg-background md:hidden">
+        <nav className="container flex items-center justify-between px-8">
+            
+            <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+                <Logo/>
+            </div>
+            <div className="flex items-center gap-2">
+                <ThemeSwitcherButton/>
+            </div>
+        </nav>
+    </div>
 }
 
 export default Navbar;
