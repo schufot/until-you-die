2a2e2c69e2d45d10ee1553895038dc07245a1094
schufot <156847189+schufot@users.noreply.github.com>
2024-08-10 15:06:33 +0200

    add datepicker

--- app/(page)/page.tsx
+++ app/(page)/page.tsx
@@ -1,8 +1,16 @@
+
+import { DatePicker } from '@/components/DatePicker'
 import React from 'react'
 
 function page() {
   return (
-    <div>page</div>
+    <div className='h-full bg-background'>
+        <div className="border-b bg-card">
+            <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+                <DatePicker/>
+            </div>
+        </div>
+    </div>
   )
 }
 
