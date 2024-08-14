683d51142d0642b3bbd5e85641c444294b60a447
schufot <156847189+schufot@users.noreply.github.com>
2024-08-14 20:02:54 +0200

    fix calendar picker to navigate by years and months

--- app/(page)/page.tsx
+++ app/(page)/page.tsx
@@ -4,6 +4,7 @@ import { Combobox } from '@/components/Combobox'
 import { DatePicker } from '@/components/DatePicker'
 import Timer from '@/components/Timer'
 import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar';
 import React, { useState } from 'react'
 
 
@@ -13,7 +14,7 @@ function page()  {
     <div className='h-full bg-background'>
         <div className="border-b bg-card">
             <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
-                <DatePicker/>
+                <Calendar captionLayout="dropdown-buttons" fromYear={1950} toYear={2050}/>
                 <Combobox></Combobox>
                 <Button variant={"outline"} className='border-violet-500 bg-violet-950 text-white hover:bg-violet-700 hover:text-white'>
                 Calculate your remaining lifetime
