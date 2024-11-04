28d30a3e7bfbd7e96fd95bed73e475998db54cf9
schufot <156847189+schufot@users.noreply.github.com>
2024-11-04 20:21:47 +0100

    fix: split deploy process

--- until-you-die/next.config.mjs
+++ until-you-die/next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: process.env.NODE_ENV === 'production' ? '/until-you-die' : '',
-  };
-  
-  export default nextConfig;
+  basePath: "/until-you-die",
+  output: "export",
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
